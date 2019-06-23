0b6d4b389c89604a53a1203932e26b2b1ab50388
Andrea <andrea.belluccia@gmail.com>
2019-06-23 16:23:39 +0200

    Added test logic to chatbot to reply

---
+++ README.md
@@ -0,0 +1,33 @@
+# SIMPLE CHATBOT APP WITH DISCORD.JS
+
+Easy Chatbot created with Node.js and Discord.js
+
+## Installation
+
+After cloning the Repository, run the command:
+
+```bash
+npm install
+```
+
+and then, run the command:
+
+```bash
+nodemon index.js
+```
+
+to start the Server.
+
+## Usage
+
+Visit 'https://discordapp.com/channels/592347385011634187/592347577131860023' and type in the following words to test it:
+
+- ping
+- hello
+- !andrea
+- !pretty
+- !test + anything else
+
+## Author
+
+### ANDREA BELLUCCIA
--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, RichEmbed } = require("discord.js");
+const client = new Client();
 
 client.on("ready", () => {
-  console.log("Bot is ready");
+  console.log(`Bot is ready as ${client.user.tag}`);
+  client.user.setStatus("online");
+  console.log(client.user.presence.status);
+
+  //   const testChannel = client.channels.find(channel => channel.name === "test");
+  //   console.log(testChannel);
+});
+
+client.on("message", async message => {
+  switch (message.content) {
+    case "ping":
+      message.reply("pong");
+      break;
+    case "hello":
+      message.channel.send(`Hello ${message.author}!`);
+      break;
+    case "!andrea":
+      message.channel.send(
+        "https://www.linkedin.com/in/andreabellucciajuniordeveloper/"
+      );
+      break;
+    case "!pretty":
+      const embed = new RichEmbed()
+        .setTitle("A Pretty Message")
+        .setColor("RED")
+        .setAuthor("Andrea", "")
+        .addField("Something One", "Lorem Ipsum");
+
+      message.channel.send(embed);
+      break;
+    case "!clear":
+      const fetchedMessages = await message.channel.fetchMessages({
+        limit: 100
+      });
+      message.channel.bulkDelete(fetchedMessages);
+    default:
+      return;
+  }
+
+  if (message.content.includes("!test")) {
+    message.channel.send("Glad you are testing!");
+  }
 });
 
 client.login("NTkyMzQ3OTk3NzY1ODk0MTU1.XQ-BJQ.tryVi798dqynmtCwdT44HICHE-A");
