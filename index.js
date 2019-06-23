06cd5295f650156365a28223249a887fba9ba31b
Andrea <andrea.belluccia@gmail.com>
2019-06-23 15:48:30 +0200

    Check bot connection with Server

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("Bot is ready");
+});
+
+client.login("NTkyMzQ3OTk3NzY1ODk0MTU1.XQ-BJQ.tryVi798dqynmtCwdT44HICHE-A");
